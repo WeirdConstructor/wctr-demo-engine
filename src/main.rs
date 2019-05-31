eb9cc56368379d28a98a11c1cc63a21ede8af995
Weird Constructor <weirdconstructor@gmail.com>
2019-05-31 21:44:21 +0200

    Worked on the drawing and layout routines

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use sdl2::event::Event;
 use sdl2::event::WindowEvent;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
+use sdl2::rect::Point;
+use chrono::offset::Utc;
+use chrono::DateTime;
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::time::{Instant, Duration};
@@ -90,22 +93,29 @@ pub struct DrawLine {
     attr:           DrawLineAttr,
 }
 
+enum ColumnSizing {
+    TextWidth(String),
+    ExpandFract(i32),
+}
+
 pub struct Column {
     head:          String,
     rows:          std::vec::Vec<String>,
-    min_width:     i32,
-    fract_width:   i32,
+    size:          ColumnSizing,
+    calc_size:     Option<i32>,
 }
 
 pub struct Table {
-    title:  String,
-    columns:  std::vec::Vec<Column>,
+    title:      String,
+    columns:    std::vec::Vec<Column>,
+    row_gap:    u32,
+    col_gap:    u32,
 }
 
 pub trait FmPage {
     fn len(&self) -> usize;
     fn as_draw_page(&self) -> Table;
-    fn is_cursor_here(&self, idx: usize) -> bool;
+    fn get_cursor_idx(&self) -> usize;
     fn is_selected(&self, idx: usize) -> bool;
     fn needs_repage(&self) -> bool;
     fn needs_redraw(&self) -> bool;
@@ -113,18 +123,20 @@ pub trait FmPage {
 
 impl FmPage for PathSheet {
     fn len(&self) -> usize { self.paths.len() }
-    fn is_cursor_here(&self, idx: usize) -> bool { idx == self.cursor_idx }
+    fn get_cursor_idx(&self) -> usize { self.cursor_idx }
     fn is_selected(&self, idx: usize) -> bool { self.selection.get(&idx).is_some() }
     fn needs_repage(&self) -> bool { self.paths_dirty }
     fn needs_redraw(&self) -> bool { self.state_dirty }
     fn as_draw_page(&self) -> Table {
         Table {
             title: String::from(self.base.to_string_lossy()),
+            row_gap: 2,
+            col_gap: 10,
             columns: vec![
                 Column {
                     head: String::from("name"),
-                    min_width: 0,
-                    fract_width: 5,
+                    size: ColumnSizing::ExpandFract(1),
+                    calc_size: None,
                     rows: self.paths.iter().map(|p| {
 
                 // Theme draus machen:
@@ -138,16 +150,17 @@ impl FmPage for PathSheet {
                 },
                 Column {
                     head: String::from("time"),
-                    min_width: 0,
-                    fract_width: 2,
-                    rows: self.paths.iter().map(|_p| {
-                        String::from("?time?")
+                    size: ColumnSizing::TextWidth(String::from("MMMM-MM-MM MM:MM:MM")),
+                    calc_size: None,
+                    rows: self.paths.iter().map(|p| {
+                        let dt : DateTime<Utc> = p.mtime.into();
+                        format!("{}", dt.format("%Y-%m-%d %H:%M:%S"))
                     }).collect(),
                 },
                 Column {
                     head: String::from("size"),
-                    min_width: 0,
-                    fract_width: 1,
+                    size: ColumnSizing::TextWidth(String::from("MMMMMM")),
+                    calc_size: None,
                     rows: self.paths.iter().map(|_p| {
                         String::from("{???}")
                     }).collect(),
@@ -195,9 +208,20 @@ impl<'a, 'b> FileManager<'a, 'b> {
                 pg.cache = Some(pg.fm_page.as_draw_page());
             }
 
+            let win_size = self.draw_state.canvas.window().size();
+            let half_width = win_size.0 / 2;
+            self.draw_state.canvas.set_draw_color(NORM_BG_COLOR);
+            self.draw_state.canvas.fill_rect(Rect::new(0, 0, win_size.0, win_size.1));
+            self.draw_state.canvas.set_draw_color(DIVIDER_COLOR);
+            self.draw_state.canvas.draw_line(
+                Point::new(half_width as i32 + 1, 0),
+                Point::new(half_width as i32 + 1, win_size.1 as i32));
+
             if pg.cache.is_some() {
                 self.draw_state.draw_table(
-                    pg.cache.as_mut().unwrap(), 0, 0, 300);
+                    pg.cache.as_mut().unwrap(),
+                    0, 0,
+                    half_width as i32);
             }
         }
     }
@@ -252,17 +276,18 @@ Basic building blocks:
     - Tracker Input
 */
 
-//const NORM_BG_COLOR  = Color::RGB( 19,  19,  19);
-//const NORM_BG2_COLOR = Color::RGB( 38,  38,  38);
-//const NORM_BG3_COLOR = Color::RGB( 51,  51,  51);
-//const NORM_FG_COLOR  = Color::RGB(229, 229, 229);
+const NORM_BG_COLOR  : Color = Color { r:  19, g:  19, b:  19, a: 0xff };
+const NORM_BG2_COLOR : Color = Color { r:  38, g:  38, b:  38, a: 0xff };
+const NORM_BG3_COLOR : Color = Color { r:  51, g:  51, b:  51, a: 0xff };
+const NORM_FG_COLOR  : Color = Color { r: 229, g: 229, b: 229, a: 0xff };
+//const NORM_FG_COLOR  : Color = Color { r: 229, g: 229, b: 0, a: 0xff };
 //const CURS_BG_COLOR  = Color::RGB(144, 238, 144);
 //const CURS_BG_COLOR  = Color::RGB(  0,   0,   0);
 //const HIGH_BG_COLOR  = Color::RGB(255,   0,   0);
 //const HIGH_FG_COLOR  = Color::RGB(  0,   0,   0);
 //const SLCT_BG_COLOR  = Color::RGB(169, 169, 169);
 //const SLCT_FG_COLOR  = Color::RGB(  0,   0,   0);
-//const DIVIDER_COLOR  = Color::RGB( 34,  69,  34);
+const DIVIDER_COLOR : Color = Color { r: 34,  g: 69,  b: 34, a: 0xff };
 //const SCRIND_COLOR   = Color::RGB( 96, 255,  96);
 //const DIR_FG_COLOR   = Color::RGB( 64, 255, 255);
 //const LNK_FG_COLOR   = Color::RGB(255, 128, 255);
@@ -282,44 +307,122 @@ impl<'a, 'b> DrawState<'a, 'b> {
         self.canvas.present();
     }
 
-    fn draw_table(&mut self, table: &Table, x_offs: i32, y_offs: i32, width: i32) {
-        let fracts : i32 = table.columns.iter().map(|c| c.fract_width).sum();
+    fn calc_column_text_widths(&mut self, table: &mut Table) {
+        for col in table.columns.iter_mut() {
+            if let ColumnSizing::TextWidth(txt) = &col.size {
+                if col.calc_size.is_none() {
+                    let tsize = self.font.borrow().size_of(&txt);
+                    col.calc_size = Some(tsize.unwrap_or((0, 0)).0 as i32);
+                } else {
+                    col.calc_size = Some(0);
+                }
+            } else {
+                col.calc_size = Some(0);
+            }
+        }
+    }
+
+    fn calc_column_width(&mut self, table: &Table, table_width: i32, skip_cols: u32) -> std::vec::Vec<i32> {
+        if skip_cols >= table.columns.len() as u32 {
+            return Vec::new();
+        }
+
+        let cols : std::vec::Vec<&Column> = table.columns.iter().rev().skip(skip_cols as usize).rev().collect();
+
+        let fixed_width : i32 =
+            cols.iter().map(|c| c.calc_size.unwrap() + table.col_gap as i32).sum();
+
+        let expand_rest_width = table_width - fixed_width;
+
+        if expand_rest_width < 0 {
+            return self.calc_column_width(table, table_width, skip_cols + 1);
+        }
+
+        let fract_sum : u32 = cols.iter().map(|c|
+            match c.size {
+                ColumnSizing::ExpandFract(f) => f as u32,
+                _ => 0u32,
+            }).sum();
+
+        cols.iter().map(|column|
+            match column.size {
+                ColumnSizing::TextWidth(_)   => column.calc_size.unwrap() + table.col_gap as i32,
+                ColumnSizing::ExpandFract(f) => ((expand_rest_width * f) / fract_sum as i32) + table.col_gap as i32,
+            }).collect()
+    }
+
+    fn draw_table(
+        &mut self, table: &mut Table,
+        x_offs: i32, y_offs: i32,
+        table_width: i32) {
+        println!("FO {}", table_width);
+
+        self.calc_column_text_widths(table);
+        let cols = self.calc_column_width(table, table_width, 0);
 
         let mut x = x_offs;
-        for column in table.columns.iter() {
-            let mut width = (width * column.fract_width) / fracts;
-            if width < column.min_width { width = column.min_width; }
+        for width_and_col in cols.iter().enumerate().zip(table.columns.iter()) {
+            let row_idx = (width_and_col.0).0;
+            let width   = (width_and_col.0).1;
+            let column  = width_and_col.1;
 
+            let row_height = self.font.borrow().height() + table.row_gap as i32;
+
+            self.canvas.set_draw_color(NORM_BG_COLOR);
+            self.canvas.fill_rect(Rect::new(x, y_offs, *width as u32, row_height as u32));
             draw_text(
                 &mut self.font.borrow_mut(),
+                NORM_FG_COLOR,
                 &mut self.canvas,
                 x,
                 y_offs,
-                width,
+                *width - table.col_gap as i32,
                 &column.head);
 
-            let mut y = y_offs + self.font.borrow().height();
+            self.canvas.set_draw_color(NORM_FG_COLOR);
+            self.canvas.draw_line(
+                Point::new(x,         y_offs + (row_height - table.row_gap as i32)),
+                Point::new(x + width, y_offs + (row_height - table.row_gap as i32)));
 
-            for row in column.rows.iter() {
+            let mut y = y_offs + row_height;
+
+            for (col_idx, row) in column.rows.iter().enumerate() {
+                if col_idx % 2 == 0 {
+                    if row_idx % 2 == 0 {
+                        self.canvas.set_draw_color(NORM_BG_COLOR);
+                    } else {
+                        self.canvas.set_draw_color(NORM_BG2_COLOR);
+                    }
+                } else {
+                    if row_idx % 2 == 0 {
+                        self.canvas.set_draw_color(NORM_BG2_COLOR);
+                    } else {
+                        self.canvas.set_draw_color(NORM_BG3_COLOR);
+                    }
+                }
+                self.canvas.fill_rect(Rect::new(x, y, *width as u32, row_height as u32));
+
                 draw_text(
                     &mut self.font.borrow_mut(),
+                    NORM_FG_COLOR,
                     &mut self.canvas,
                     x,
                     y,
-                    width,
+                    *width - table.col_gap as i32,
                     &row);
-                y += self.font.borrow().height();
+                y += row_height;
             }
 
             x += width;
+            println!("X= {}", x);
         }
     }
 }
 
-fn draw_text(font: &mut sdl2::ttf::Font, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, x: i32, y: i32, max_w: i32, txt: &str) {
+fn draw_text(font: &mut sdl2::ttf::Font, color: Color, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, x: i32, y: i32, max_w: i32, txt: &str) {
     let txt_crt = canvas.texture_creator();
 
-    let sf = font.render(txt).blended(Color::RGBA(0, 0, 0, 255)).map_err(|e| e.to_string()).unwrap();
+    let sf = font.render(txt).blended(color).map_err(|e| e.to_string()).unwrap();
     let mut txt = txt_crt.create_texture_from_surface(&sf).map_err(|e| e.to_string()).unwrap();
     let tq = txt.query();
 
@@ -351,10 +454,11 @@ pub fn main() -> Result<(), String> {
 
     let ttf_ctx = sdl2::ttf::init().map_err(|e| e.to_string())?;
 
-    let mut font = ttf_ctx.load_font("DejaVuSansMono.ttf", 12).map_err(|e| e.to_string())?;
+    let mut font = ttf_ctx.load_font("DejaVuSansMono.ttf", 14).map_err(|e| e.to_string())?;
 //    font.set_style(sdl2::ttf::FontStyle::BOLD | sdl2::ttf::FontStyle::UNDERLINE);
-    font.set_hinting(sdl2::ttf::Hinting::Mono);
-    font.set_kerning(false);
+    font.set_hinting(sdl2::ttf::Hinting::Normal);
+//    font.set_outline_width(0.1);
+    font.set_kerning(true);
 
     let mut fm = FileManager {
         draw_state: DrawState {
@@ -388,9 +492,10 @@ pub fn main() -> Result<(), String> {
 
         fm.draw_state.clear();
 
-        let pth = std::path::Path::new("..");
+        let pth = std::path::Path::new(".");
 //        let ps = PathSheet::read(pth).unwrap();
 //        let dp = ps.as_draw_page();
+        println!("WIN WIDTH: {}", fm.draw_state.canvas.window().size().0);
         fm.open_path_in(pth, PanePos::LeftTab);
         fm.redraw();
 //        ds.draw_page(&dp);
